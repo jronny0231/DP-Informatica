ac2ada45564a22c592f15b91fe1abedf5ebce38b
JRonny0231 <jr.martinezvaldez@gmail.com>
2021-10-06 23:27:09 -0400

    Laravel/Jetstream etapa de rutas a controladores a vistas, usando Bootstrap en la vista

---
+++ resources/views/inicioVista.blade.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+
+    <div class="container">
+    <h1>Ejemplo \ <span class="badge bg-secondary">Inicio</span></h1>
+        <div class="card" style="width: 18rem;">
+            <img src="https://www.etondigital.com/wp-content/uploads/2019/05/laravel-blog.png" class="card-img-top" alt="Laravel">
+            <div class="card-body">
+                <h5 class="card-title">Jersson Ronny</h5>
+                <p class="card-text">Programador novato en el area de web.</p>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">/Facebook</li>
+                <li class="list-group-item">/Twitter</li>
+                <li class="list-group-item">/Linkedin</li>
+            </ul>
+            <div class="card-body">
+                <a href="#" class="card-link">login</a>
+                <a href="#" class="card-link">register</a>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-kQtW33rZJAHjgefvhyyzcGF3C5TFyBQBA13V1RKPf4uH+bwyzQxZ6CmMZHmNBEfJ" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.min.js" integrity="sha384-PsUw7Xwds7x08Ew3exXhqzbhuEYmA2xnwc8BuD6SEr+UmEHlX8/MCltYEodzWA4u" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
